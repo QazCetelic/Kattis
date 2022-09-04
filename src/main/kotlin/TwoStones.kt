e8d416cb99b1fb5ef95a51db47b7b4330549d9e8
ಠ_ಠ <qaz.cetelic@protonmail.com>
2022-09-04 17:25:35 +0200

    Add TwoStones problem solution

---
+++ src/main/kotlin/TwoStones.kt
@@ -0,0 +1,12 @@
+fun main() {
+    input {
+        val isEven = it.toInt() % 2 == 0
+        if (isEven) {
+            println("Bob")
+        }
+        else {
+            println("Alice")
+        }
+    }
+}
+
